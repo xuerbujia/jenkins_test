799a64a6ce233ae85f5ca47d76f99a25b4eacf88
xuerbujia <1056646612@qq.com>
2022-09-16 00:04:21 +0800

    test

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{"hello": "world"})
+	})
+	r.Run(":8081")
+}
