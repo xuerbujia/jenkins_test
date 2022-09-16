621675b2b69d96710a6b243f96ddb48b578da698
xuerbujia <1056646612@qq.com>
2022-09-16 13:52:57 +0800

    test

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	//r := gin.Default()
-	//r.GET("/", func(context *gin.Context) {
-	//	context.JSON(http.StatusOK, gin.H{"hello": "world"})
-	//})
-	//r.Run(":8081")
-	fmt.Println("hello world ")
+	r := gin.Default()
+	r.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{"hello": "world"})
+	})
+	r.Run(":8081")
+
 }
